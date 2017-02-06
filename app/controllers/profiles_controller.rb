7937b0d19f348996a4a2441570460eca3a373adb
Josephina Bos <josephinabos@gmail.com>
2017-02-06 16:34:40 +0100

    add sign up form styling

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,43 @@
 class ProfilesController < ApplicationController
-  def index
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
+    @user = @profile.user
   end
 
   def edit
+    @user = @profile.user
   end
 
   def update
+    @profile.update(profile_params)
+    if @profile.save
+      redirect_to profile_path(@profile)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:bio)
+  end
+
+  def find_profile
+    @profile = Profile.find(params[:id])
   end
 end
